b0fcf6b185fcd57f5e4be118c80814b4f162e58d
amrutgawade <amrutgawade.23@gmail.com>
2024-08-10 18:14:26 +0530

    feat: Initialized SpringBoot Project

---
+++ src/test/java/com/freakdevs/Hostelian/HostelianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freakdevs.Hostelian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
